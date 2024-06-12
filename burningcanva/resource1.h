866a9d5f72b98cbafbd515a3ad274720b04b3f96
QYXQYXQYX1 <1421992892@qq.com>
2024-06-12 16:30:32 +0800

    修改了文件资源路径

--- burningcanva/resource1.h
+++ burningcanva/resource1.h
@@ -38,13 +38,15 @@
 #define IDR_JPG13                       138
 #define IDR_JPG14                       139
 #define IDR_JPG15                       140
+#define IDR_JPG16                       141
+#define IDR_JPG17                       142
 #define ID_ACCELERATOR40002             40002
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        141
+#define _APS_NEXT_RESOURCE_VALUE        143
 #define _APS_NEXT_COMMAND_VALUE         40004
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
